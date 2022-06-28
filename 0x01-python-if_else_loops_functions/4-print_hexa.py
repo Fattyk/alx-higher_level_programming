5038dc76e7688e35be9fcc3d6cbf93fbb7e76b6c
Fattyk <ogundele.fatai.k@gmail.com>
2022-06-28 21:27:15 +0000

    program that prints all numbers from 0 to 98 in decimal and in hexadecimal

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -2,4 +2,4 @@
 '''program that prints all numbers from 0 to 98 in decimal and in hexadecimal'''
 
 for item in range(0, 99):
-    print("{0:d} = {0:x}".format(item))
+    print("{0:d} = 0x{0:x}".format(item))
