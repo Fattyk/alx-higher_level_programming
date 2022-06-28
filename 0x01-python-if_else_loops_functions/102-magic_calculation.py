7b94c6e3b782e43bd8dfcdf46b8cbc0c1137c5df
Fattyk <ogundele.fatai.k@gmail.com>
2022-06-28 23:28:50 +0000

    Python bytecode

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''Python bytecode'''
+
+
+def magic_calculation(a, b, c):
+   '''Python bytecode'''
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
