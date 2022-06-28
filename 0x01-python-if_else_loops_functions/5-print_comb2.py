c2ecbb95ed743d9214307561c8b91a18e883d054
Fattyk <ogundele.fatai.k@gmail.com>
2022-06-28 22:15:39 +0000

    program that prints all possible different combinations of two digits

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''prints 0 to 99'''
 
-for item in range(0, 100):
-    print("{0:02d}, ".format(item), end="")
-print("{}".format('\n'))
+for item in range(0, 99):
+    print("{0:2d}".format(item), end=", ")
+print("{}".format(99))
