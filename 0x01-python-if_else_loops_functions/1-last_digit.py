8157dfd9e702186b9585b6d45dcb458ee0345903
Fattyk <ogundele.fatai.k@gmail.com>
2022-06-28 16:21:20 +0000

    Add a program that checks the condition of last digit

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number < 0:
+    sign = -1
+    number *= sign
+else:
+    sign = 1
+end = number % 10
+end *= sign
+if (end > 5):
+    print(f"Last digit of {number:d} is {end:d} and is greater than 5")
+elif (end == 0):
+    print(f"Last digit of {number:d} is {end:d} and is 0")
+elif ((end < 6) and (end != 0)):
+    print(f"Last digit of {number:d} is {end:d} and is less than 6 and not 0")
