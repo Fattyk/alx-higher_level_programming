afda83321c60073d2ecbac50013eec997f17bd27
Fattyk <ogundele.fatai.k@gmail.com>
2022-06-28 22:45:37 +0000

    Add a function that prints a string in uppercase followed by a new line

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''a function that checks for lowercase character'''
 
+
 def islower(c):
     """Function checks for lowercase characters."""
     if ((ord(c) > 96) and (ord(c) < 123)):
