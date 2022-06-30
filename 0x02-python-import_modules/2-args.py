9156c5cc662a2671eec2b896e7ff62af70adcc94
Fattyk <ogundele.fatai.k@gmail.com>
2022-06-30 23:10:11 +0000

    Add a program that prints the number of and the list of its arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """program that prints the number of and the list of its arguments"""
+    import sys
+    argv = sys.argv
+    argc = len(sys.argv)
+    if argc == 1:
+        print("{} arguments.".format(argc - 1))
+    else:
+        if argc == 2:
+            print("{} argument:".format(argc - 1))
+            print("{}: {}".format(argc - 1, argv[argc-1]))
+        else:
+            print("{} arguments.".format(argc - 1))
+            for i in range(1, argc):
+                print("{}: {}".format(i, argv[i]))
