ed85078ddc8928a8d95297b4509ad520cae41aff
Fattyk <ogundele.fatai.k@gmail.com>
2022-07-29 01:09:20 +0000

    a function that returns a tuple with the length of a string and its first character.

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    result = ()
     first = turple_a + (0, 0)
     second = turple_b + (0, 0)
     result = first[0] + second[0], first[1] + second[1]
