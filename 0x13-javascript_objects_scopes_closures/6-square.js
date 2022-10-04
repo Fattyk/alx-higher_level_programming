38b1223787499a13d48f6d0ca337674232acf9e1
Fattyk <ogundele.fatai.k@gmail.com>
2022-10-04 15:48:33 -0700

    Add a class Square that defines a square and inherits from Square of 5-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+const OldSquare = require('./5-square');
+
+class Square extends OldSquare {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let k = '';
+      for (let j = 0; j < this.width; j++) {
+        k += c;
+      }
+      console.log(k);
+    }
+  }
+}
+
+module.exports = Square;
