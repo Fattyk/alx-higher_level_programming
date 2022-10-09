a0a98930a6ed398ecbac8dc97256a3e8b0277fec
Fattyk <ogundele.fatai.k@gmail.com>
2022-10-09 14:35:55 -0700

    Updated task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,47 +1,18 @@
 #!/usr/bin/python3
-"""Using sub queries.
-
-This module illustrates how to get the list of cities from given states
-"""
-import re
-import sys
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
-
-
-def print_states(user, password, name, search):
-    """Print the list of cities in a given states in database"""
-    try:
-        host = "localhost"
-        port = 3306
-        db = MySQLdb.connect(
-            host=host, port=port, user=user,
-            passwd=password, db=name
-        )
-        cur = db.cursor()
-        cur.execute("""SELECT cities.name FROM
-                cities INNER JOIN states ON states.id=cities.state_id
-                WHERE states.name=%s""", (search)
-        states = cur.fetchall()
-    except Exception as e:
-        print("Error: ", str(e))
-        return
-
-    for state in states:
-        print(state[0], end=", ") if state != states[-1] else print(state[0])
-
-
-def main():
-    """Entry point for the whole program"""
-    args = sys.argv[1:]
-    if len(args) != 4:
-        print("username, password, dbname and search are required")
-        return
-    if re.search("[;'\"]", args[3]):
-        print("Invalid pattern")
-        return
-
-    print_states(args[0], args[1], args[2], args[3])
+import sys
 
 
 if __name__ == "__main__":
-    main()
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("""SELECT cities.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id
+                WHERE states.name=%s""", (sys.argv[4],))
+    rows = cur.fetchall()
+    tmp = list(row[0] for row in rows)
+    print(*tmp, sep=", ")
+    cur.close()
+    db.close()
