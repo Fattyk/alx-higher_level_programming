d2390105b5864b573e5fdc04b2a3226ec2c269f9
Fattyk <ogundele.fatai.k@gmail.com>
2022-10-20 21:22:08 -0700

    Update task 4

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a GET request to a URL with header variable X-School-User-Id and value 98
-curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
+curl -sXH "X-HolbertonSchool-User-Id: 98" "${1}"
