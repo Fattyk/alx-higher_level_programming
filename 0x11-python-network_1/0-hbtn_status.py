31a3e4bdb7ac7d768dc8799e13c9b215568b61b8
Fattyk <ogundele.fatai.k@gmail.com>
2022-10-21 13:08:31 -0700

    Add a Python script that fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# Python script that fetches https://alx-intranet.hbtn.io/status
+import urllib.request
+
+def fetch_url():
+    '''This method fetchs https://alx-intranet.hbtn.io/status using urllib'''
+    with urllib.request.urlopen(
+            "https://alx-intranet.hbtn.io/status"
+    ) as response:
+        return response.read()
+
+def main():
+    '''Main Entry - This is the main entry of the program'''
+    body = fetch_url()
+    print(f"\t- type: {type(body)}")
+    print(f"\t- content: {body}")
+    print(f"\t- utf8 content: {body.decode(encoding='utf-8')}")
+
+if __name__ == "__main__":
+    main()
