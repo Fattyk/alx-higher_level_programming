0d2473ca4cfab3a8e464693b7321b3c21339ff97
Fattyk <ogundele.fatai.k@gmail.com>
2022-11-15 13:47:32 +0000

    a script that reads and prints the content of a file.

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // a script that reads and prints the content of a file.
 const fs = require('fs');
-const file = process.argv[2]
+const file = process.argv[2];
 
-fs.readFile(file, 'utf8', function(error, data) {
-    console.log(error || data);
+fs.readFile(file, 'utf8', function (error, data) {
+  console.log(error || data);
 });
