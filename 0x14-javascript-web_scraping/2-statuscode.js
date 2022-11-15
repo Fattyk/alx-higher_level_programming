2cfe4cbd28f896047cbe88abd7815529939c541e
Fattyk <ogundele.fatai.k@gmail.com>
2022-11-15 15:24:27 +0000

    a script that display the status code of a GET request.

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,5 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, function (error, response, body) {
-  error? console.log('code: 404') : console.log('code:', response && response.statusCode);
+  error ? console.log('code: 404') : console.log('code:', response && response.statusCode);
 });
